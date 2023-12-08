a3b1b59722e01c101d44a88f7037cb55cf3eb9b8
Kacper Martela <kacper99@hotmail.co.uk>
2023-12-08 22:26:02 +0000

    refactor: left pad challenge name with 0s to order numerically

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,11 @@
 rootProject.name = "jvm-leetcode"
 
 include(
-        "1-2-sum",
-        "3-longest-substring-without-repeating-characters",
-        "11-container-with-most-water",
-        "15-3-sum",
-        "121-best-time-to-buy-sell-stock",
-        "125-valid-palindrome",
-        "167-2-sum-sorted",
+        "0001-2-sum",
+        "0003-longest-substring-without-repeating-characters",
+        "0011-container-with-most-water",
+        "0015-3-sum",
+        "0121-best-time-to-buy-sell-stock",
+        "0125-valid-palindrome",
+        "0167-2-sum-sorted",
 )
