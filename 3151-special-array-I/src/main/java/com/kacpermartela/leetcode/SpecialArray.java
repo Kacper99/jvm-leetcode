ae9d48bc4c0a91d49f42d57fefcf0e52d5deeb3f
Kacper Martela <kacper99@hotmail.co.uk>
2025-02-01 17:58:19 +0000

    refactor(3151): special array I simpler

--- 3151-special-array-I/src/main/java/com/kacpermartela/leetcode/SpecialArray.java
+++ 3151-special-array-I/src/main/java/com/kacpermartela/leetcode/SpecialArray.java
@@ -4,7 +4,7 @@ public class SpecialArray {
 
     public boolean isArraySpecial(int[] nums) {
         for (int i = 0; i < nums.length - 1; i++) {
-            if ((nums[i] % 2 == 0 && nums[i + 1] % 2 == 0) || (nums[i] % 2 == 1 && nums[i + 1] % 2 == 1)) {
+            if (nums[i] % 2 == nums[i + 1] % 2) {
                 return false;
             }
         }
