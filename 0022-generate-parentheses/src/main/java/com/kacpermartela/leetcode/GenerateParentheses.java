beb78b4cc082b07c8fa323bd008caf6bfa829593
Kacper Martela <kacper99@hotmail.co.uk>
2025-02-09 14:16:36 +0000

    refactor(22): generate parentheses optimisation

--- 0022-generate-parentheses/src/main/java/com/kacpermartela/leetcode/GenerateParentheses.java
+++ 0022-generate-parentheses/src/main/java/com/kacpermartela/leetcode/GenerateParentheses.java
@@ -2,8 +2,6 @@ package com.kacpermartela.leetcode;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Stack;
 
 public class GenerateParentheses {
 
@@ -15,39 +13,15 @@ public class GenerateParentheses {
 
     public static void helper(
             int n, List<String> results, String combination, int openingBrackets, int closingBrackets) {
+        System.out.println(combination);
         if (combination.length() == n * 2) {
-            if (isValid(combination)) {
-                results.add(combination);
-            }
+            results.add(combination);
         }
         if (openingBrackets < n) {
             helper(n, results, combination + "(", openingBrackets + 1, closingBrackets);
         }
-        if (closingBrackets < n) {
+        if (closingBrackets < openingBrackets) {
             helper(n, results, combination + ")", openingBrackets, closingBrackets + 1);
         }
-    }
-
-    private static final Map<Character, Character> CHAR_PAIRS = Map.of(')', '(');
-
-    public static boolean isValid(String s) {
-        if (s.length() % 2 == 1) return false;
-
-        Stack<Character> stack = new Stack<>();
-
-        for (char c : s.toCharArray()) {
-            if (CHAR_PAIRS.containsValue(c)) {
-                stack.push(c);
-            } else if (CHAR_PAIRS.containsKey(c) && !stack.isEmpty()) {
-                char poppedChar = stack.pop();
-                if (CHAR_PAIRS.get(c) != poppedChar) {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        }
-
-        return stack.isEmpty();
     }
 }
