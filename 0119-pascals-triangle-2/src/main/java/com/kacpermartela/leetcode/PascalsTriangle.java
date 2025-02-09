f387b24d3a6166c81c70816e627151baa1a4a64e
Kacper Martela <kacper99@hotmail.co.uk>
2025-02-09 19:48:58 +0000

    refactor(119): pascals triangle 2
    only store the previous row

--- 0119-pascals-triangle-2/src/main/java/com/kacpermartela/leetcode/PascalsTriangle.java
+++ 0119-pascals-triangle-2/src/main/java/com/kacpermartela/leetcode/PascalsTriangle.java
@@ -6,26 +6,25 @@ import java.util.List;
 public class PascalsTriangle {
 
     public static List<Integer> getRow(int rowIndex) {
-        final var rows = new ArrayList<List<Integer>>();
+        List<Integer> prevRow = new ArrayList<Integer>();
         for (int i = 0; i < rowIndex + 1; i++) {
             if (i == 0) {
-                rows.add(List.of(1));
+                prevRow = List.of(1);
             } else if (i == 1) {
-                rows.add(List.of(1, 1));
+                prevRow = List.of(1, 1);
             } else {
                 List<Integer> row = new ArrayList<>();
                 row.add(1);
                 for (int j = 1; j < i; j++) {
-                    final var prevRow = rows.get(i - 1);
                     final var upAndLeft = prevRow.get(j - 1);
                     final var upAndRight = prevRow.get(j);
                     row.add(upAndLeft + upAndRight);
                 }
                 row.add(1);
-                rows.add(row);
+                prevRow = row;
             }
         }
 
-        return rows.get(rows.size() - 1);
+        return prevRow;
     }
 }
