34b4ec9f5439ba89389e788cd436874105e49540
Kacper Martela <kacper99@hotmail.co.uk>
2025-02-18 19:09:20 +0000

    docs(235): lowest common ancestor of a binary search tree

--- 0235-lca-of-a-bst/src/main/java/com/kacpermartela/leetcode/LowestCommonAncestor.java
+++ 0235-lca-of-a-bst/src/main/java/com/kacpermartela/leetcode/LowestCommonAncestor.java
@@ -14,20 +14,15 @@ package com.kacpermartela.leetcode;
 public class LowestCommonAncestor {
 
     public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        TreeNode[] res = new TreeNode[1];
-        helper(root, p, q, res);
-        return res[0];
-    }
-
-    public static void helper(TreeNode root, TreeNode p, TreeNode q, TreeNode[] res) {
-        if (root == null) {
-            return;
-        }
-        if ((root.val >= p.val && root.val <= q.val) || (root.val <= p.val && root.val >= q.val)) {
-            res[0] = root;
-        } else {
-            helper(root.left, p, q, res);
-            helper(root.right, p, q, res);
+        if (Math.max(p.val, q.val)
+                < root.val) { // If both p and q are less than the root then they LCA must be somewhere on the left side
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (Math.min(p.val, q.val)
+                > root.val) { // If both p and q are greater than the root then they LCA must be somewhere on the right
+            // side
+            return lowestCommonAncestor(root.right, p, q);
+        } else { // Otherwise, we're at the lowest common ancestor
+            return root;
         }
     }
 }
